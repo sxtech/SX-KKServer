b5de59043064728a701baf91693bb5a5cc9b9e5b
SmellyCat <1282399562@qq.com>
2019-10-24 11:23:13 +0800

    添加 设备序号

--- app/models.py
+++ app/models.py
@@ -25,10 +25,12 @@ class VehiclePass(db.Model):
     uuid = db.Column(db.String(32), default='')
     create_time = db.Column(
         db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
+    serialno = db.Column(db.String(128), default='')
 
     def __init__(self, plate_no='-', plate_color='1', pass_time=None,
                  site_id=1, direction='1', status='1', pic1='', pic2='',
-                 pic3='', pic4='', ip_addr='', device_name='', uuid='', create_time=None):
+                 pic3='', pic4='', ip_addr='', device_name='', uuid='', 
+                 create_time=None, serialno=''):
         self.plate_no = plate_no
         if pass_time is None:
             self.pass_time = arrow.now('PRC').datetime.replace(tzinfo=None)
@@ -48,9 +50,11 @@ class VehiclePass(db.Model):
             self.create_time = arrow.now('PRC').datetime.replace(tzinfo=None)
         else:
             self.create_time = create_time
+        self.serialno = serialno
         
     def __repr__(self):
         return '<vehicle_pass %r>' % self.id
+
 
 class DeviceState(db.Model):
     """相机设备状态"""
@@ -60,24 +64,42 @@ class DeviceState(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ip = db.Column(db.String(16), default='')
     serialno = db. Column(db.String(128), default='')
+    device_name = db.Column(db.String(128), default='')
     white_list = db.Column(db.String(256), default='[]')
+    stat_code = db.Column(db.Integer, default=0)
+    long = db.Column(db.Numeric(6, 6), default=0.0)
+    lat = db.Column(db.Numeric(6, 6), default=0.0)
+    create_time = db.Column(
+        db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
     last_modify = db.Column(
         db.DateTime, default=arrow.now('PRC').datetime.replace(tzinfo=None))
     update_flag = db.Column(db.Integer, default=0)
+    del_flag = db.Column(db.Integer, default=0)
 
-    def __init__(self, ip='', serialno='', white_list='[]',
-                 last_modify=None, update_flag=0):
+    def __init__(self, ip='', serialno='', device_name='', white_list='[]',
+                 stat_code=0, long=0.0, lat=0.0, create_time=None, 
+                 last_modify=None, update_flag=0, del_flag=0):
         self.ip = ip
         self.serialno = serialno
+        self.device_name = device_name
         self.white_list = white_list
+        self.stat_code = stat_code
+        self.long = long
+        self.lat = lat
+        if create_time is None:
+            self.create_time = arrow.now('PRC').datetime.replace(tzinfo=None)
+        else:
+            self.create_time = create_time
         if last_modify is None:
             self.last_modify = arrow.now('PRC').datetime.replace(tzinfo=None)
         else:
             self.last_modify = last_modify
         self.update_flag = update_flag
+        self.del_flag = del_flag
 
     def __repr__(self):
         return '<device_state %r>' % self.id
+
 
 class VehicleManage(db.Model):
     """白名单管理"""
