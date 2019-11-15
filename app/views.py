219a906afe55fcdbd0beb5808f882201d8d74b37
SmellyCat <1282399562@qq.com>
2019-11-15 20:41:49 +0800

    删除 白名单

--- app/views.py
+++ app/views.py
@@ -77,84 +77,35 @@ def get_device_state_by_ip(ip, serialno, white_list='[]'):
     dev = DeviceState.query.filter_by(ip_addr=ip).first()
     if dev is None:
         n = arrow.now('PRC').datetime.replace(tzinfo=None)
-        dev2 = DeviceState(ip_addr=ip, serialno=serialno, white_list=white_list,
+        new_dev = DeviceState(ip_addr=ip, serialno=serialno, white_list=white_list,
                     create_time=n, last_modify=n, update_flag=1)
-        db.session.add(dev2)
+        db.session.add(new_dev)
         db.session.commit()
-        return (dev2.white_list, dev2.last_modify,  dev2.update_flag)
+        return (new_dev.white_list, new_dev.last_modify,  new_dev.update_flag)
+    if dev.serialno != serialno:
+        dev.serialno = serialno
+        dev.last_modify = arrow.now('PRC').datetime.replace(tzinfo=None)
+        db.session.commit()
     return (dev.white_list, dev.last_modify,  dev.update_flag)
 
-@cache.memoize(60)
-def get_white_list():
-    white_list = VehicleManage.query.filter_by(del_flag=0).all()
-    w = {
-        'addWhiteList': {
-            'add_data': []
-        }
-    }
-    n = arrow.now('PRC')
-    st = n.format('YYYYMMDDHHmmss')
-    et = n.shift(months=3).format('YYYYMMDDHHmmss')
-    for i in white_list:
-        info = {
-            'carnum': i.plate_no,
-            'startime': st,
-            'endtime': et
-        }
-        w['addWhiteList']['add_data'].append(info)
-    return w
 
-# 更新标记设置为1
-def update_device_state_flag_by_ip(ip, update_flag=-1, white_list='[]'):
-    dev = DeviceState.query.filter_by(ip_addr=ip).first()
-    if update_flag != -1:
-        dev.update_flag = update_flag
-    if white_list != '[]':
-        dev.white_list = json.dumps(white_list)
-        dev.last_modify = arrow.now('PRC').datetime.replace(tzinfo=None)
+# 添加http信息
+def add_vehicle_http(ip, info):
+    n = arrow.now('PRC').datetime.replace(tzinfo=None)
+    v = VehicleHttp(ip_addr=ip, info=info, create_time=n)
+    db.session.add(v)
     db.session.commit()
 
 @app.route('/heart', methods=['POST'])
 def heart_post():
     #if not request.json:
     #    return jsonify({'message': 'Problems parsing JSON'}), 415
-    # 接收的json格式错误 设置为false
-    r_j_error = False
-    try:
-        if not request.json:
-            return jsonify({'message': 'Problems parsing JSON'}), 415
-    except Exception as e:
-        logger.error(e)
-        r_j_error = True
-
-    # 如果收到的json格式错误则直接获取POST请求的数据
-    try:
-        if r_j_error:
-            logger.info(request.get_data())
-            logger.info(request.get_data().decode("gbk"))
-            r = json.loads(request.get_data().decode("gbk"))
-            if r.get('Response_whiteList', None) is not None:
-                update_device_state_flag_by_ip(request.headers.get("X-Real-IP", request.remote_addr), -1, r['Response_whiteList']['data'])
-            return jsonify({}), 201, {'Content-Type': 'application/json'}
-    except Exception as e:
-        logger.error(e)
-
     try:
         result = '{}'
         logger.info(request.json)
+        add_vehicle_http(request.headers.get("X-Real-IP", request.remote_addr), request.json)
         if request.json.get('heartbeat', None) is not None:
             dev = get_device_state_by_ip(request.headers.get("X-Real-IP", request.remote_addr), request.json['heartbeat']['serialno'])
-            if dev[2] == 0:
-                # 更新前先删除旧白名单数据
-                result = json.dumps(app.config['WHITE_LIST_DEL'])
-        # 添加白名单后更新状态
-        if request.json.get('Response_AddWhiteList', None) is not None:
-            update_device_state_flag_by_ip(request.headers.get("X-Real-IP", request.remote_addr), 1)
-            result = json.dumps(app.config['WHITE_LIST_QUE'])
-        # 更新白名单数据
-        if request.json.get('Response_DeleteWhiteList', None) is not None:
-            result = json.dumps(get_white_list(), ensure_ascii=False).encode("gbk")
-            logger.info(result)
     except Exception as e:
         logger.error(e)
     return result, 201, {'Content-Type': 'application/json'}
