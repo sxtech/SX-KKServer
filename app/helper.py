4692f02c042be257e61dd5b7c96da380d65c93a7
SmellyCat <1282399562@qq.com>
2019-11-16 21:44:46 +0800

    添加 influxdb写入信息

--- app/helper.py
+++ app/helper.py
@@ -1,5 +1,9 @@
 ﻿# -*- coding: utf-8 -*-
+import requests
 
 # ip地址转整数
 def ip2int(ip):
     return struct.unpack('!I', socket.inet_aton(ip))[0]
+
+def write_info(url, data):
+    return requests.post(url, data=data)
