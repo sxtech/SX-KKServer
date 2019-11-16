7a82c4dde213affbd43a378a91245e85c24cb4a9
SmellyCat <1282399562@qq.com>
2019-11-16 21:45:55 +0800

    添加 influxdb url 地址

--- app/config.py
+++ app/config.py
@@ -6,10 +6,10 @@ class Config(object):
     # 服务器名称 string
     HEADER_SERVER = 'SX-KKServer'
     # 数据库连接 string
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123654789@127.0.0.1:54321/site'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123@127.0.0.1:54321/site'
     # 数据库连接 dict
     SQLALCHEMY_BINDS = {
-        'env': 'mysql+pymysql://root:123@127.0.0.1:3306/environ'
+        'env': 'mysql+pymysql://root:123@127.0.0.1:3307/environ'
     }
     # 连接池大小 int
     #SQLALCHEMY_POOL_SIZE = 20
@@ -18,15 +18,7 @@ class Config(object):
     # 基础URL路径
     BASE_URL_PATH = 'http://112.91.72.23:8099/images'
     # 白名单车辆查询
-    WHITE_LIST_QUE = {
-        'whiteList': {
-            'queryNumList': 1000,
-            'listPosition': 0
-        }
-    }
-    WHITE_LIST_DEL =  {
-        'deleteWhiteListAll': 1
-    }
+    INFLUXDB_URL = 'http://localhost:8086/write?db=mz'
 
 
 class Develop(Config):
