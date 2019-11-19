09b502cc5908c85dcb87d016bbd9733c49770bfa
SmellyCat <1282399562@qq.com>
2019-11-19 14:37:29 +0800

    修改 mysql表保存上传车辆图片

--- app/views.py
+++ app/views.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 import json
-import base64 
-import socket
-import struct
 import uuid
+import base64
 
 import arrow
 from flask import g, request, make_response, jsonify, abort
@@ -20,10 +18,10 @@ def index_get():
         'upload': '%supload' % (request.url_root),
         'heart': '%sheart' % (request.url_root)
     }
-    header = {'Cache-Control': 'public, max-age=60, s-maxage=60'}
+    header = {'Cache-Control': 'public, max-age=60'}
     return jsonify(result), 200, header
 
-# ±£¥ÊÕº∆¨
+# ‰øùÂ≠òÂõæÁâá
 def save_img(path, name, data):
     if not os.path.isdir(path):
         os.makedirs(path)
@@ -33,6 +31,14 @@ def save_img(path, name, data):
     file.close()
     return imgname
 
+# Ê†πÊçÆipËé∑ÂèñËÆæÂ§áÁä∂ÊÄÅ‰ø°ÊÅØ
+@cache.memoize(60)
+def get_device_state_by_ip(ip):
+    dev = DeviceState.query.filter_by(ip_addr=ip).first()
+    if dev is None:
+        return None
+    return {'stat_code': dev.stat_code, 'vehicle_point_no': dev.vehicle_point_no, 'direction': dev.direction}
+
 @app.route('/upload', methods=['POST'])
 def upload_post():
     if not request.json:
@@ -41,27 +47,45 @@ def upload_post():
         uu_id = str(uuid.uuid4())
         plate_no = request.json['AlarmInfoPlate']['result']['PlateResult']['license']
         plate_color = request.json['AlarmInfoPlate']['result']['PlateResult']['colorType']
-        #t = request.json['AlarmInfoPlate']['result']['PlateResult']['timeStamp']['Timeval']['sec']
-        ms = request.json['AlarmInfoPlate']['result']['PlateResult']['timeStamp']['Timeval']['usec']
         pass_time = arrow.get(request.json['AlarmInfoPlate']['result']['PlateResult']['timeStamp']['Timeval']['sec']).to('Asia/Shanghai')
         ip_addr = request.json['AlarmInfoPlate']['ipaddr']
         device_name = request.json['AlarmInfoPlate']['deviceName']
         serialno = request.json['AlarmInfoPlate']['serialno']
         path_seq = (app.config['BASE_PATH'], 'Plate', pass_time.format('YYYYMMDD'), serialno, pass_time.format('HH'))
         img_path = '/'.join(path_seq)
-        name = '{0}_{1}_{2}'.format(pass_time.format('YYYYMMDDHHmmss'), ms, helper.ip2int(ip_addr))
+        name = '{0}_{1}_{2}'.format(pass_time.format('YYYYMMDDHHmmss'), serialno, helper.ip2int(ip_addr))
         pic1 = save_img(img_path, name, request.json['AlarmInfoPlate']['result']['PlateResult']['imageFile']).replace(app.config['BASE_PATH'], app.config['BASE_URL_PATH'])
         pic2 = save_img(img_path, name+'_plate', request.json['AlarmInfoPlate']['result']['PlateResult']['imageFragmentFile']).replace(app.config['BASE_PATH'], app.config['BASE_URL_PATH'])
-        vehicle = VehiclePass(plate_no=plate_no, plate_color=plate_color,
-                       pass_time=pass_time.datetime, site_id='1',
-                       pic1=pic1, pic2=pic2, ip_addr=ip_addr,
-                       device_name=device_name, uuid=uu_id,
-                       serialno=serialno)
-        db.session.add(vehicle)
+        try:
+            vehicle = VehiclePass(plate_no=plate_no, plate_color=plate_color,
+                           pass_time=pass_time.datetime, site_id='1',
+                           pic1=pic1, pic2=pic2, ip_addr=ip_addr,
+                           device_name=device_name, uuid=uu_id,
+                           serialno=serialno)
+            db.session.add(vehicle)
+            db.session.commit()
+        except Exception as e:
+             logger.error(e)
+             db.session.rollback()
+        dev = get_device_state_by_ip(ip_addr)
+        if dev is None:
+            stat_code = 0
+            vehicle_point_no = 1
+            direction = 1
+        else:
+            stat_code = dev['stat_code']
+            vehicle_point_no = dev['vehicle_point_no']
+            direction = dev['direction']
+        vehicle2 = VehiclePass2(plate_no=plate_no, plate_color=plate_color,
+                        pass_time=pass_time.datetime, stat_code=stat_code,
+                        vehicle_point_no=vehicle_point_no, direction=direction,
+                        pic1=pic1, pic2=pic2, ip_addr=ip_addr, device_name=device_name,
+                        uuid=uu_id, serialno=serialno)
+        db.session.add(vehicle2)
         db.session.commit()
 
     except Exception as e:
-        logger.exception(e)
+        logger.error(e)
         return jsonify({}), 201
     result = {
         "Response_AlarmInfoPlate": {
@@ -73,7 +97,7 @@ def upload_post():
     return jsonify(result), 201
 
 #@cache.memoize(60)
-def get_device_state_by_ip(ip, serialno, white_list='[]'):
+def set_device_state_by_ip(ip, serialno, white_list='[]'):
     dev = DeviceState.query.filter_by(ip_addr=ip).first()
     if dev is None:
         n = arrow.now('PRC').datetime.replace(tzinfo=None)
@@ -89,25 +113,17 @@ def get_device_state_by_ip(ip, serialno, white_list='[]'):
     return (dev.white_list, dev.last_modify,  dev.update_flag)
 
 
-# ÃÌº”http–≈œ¢
-def add_vehicle_http(ip, info):
-    n = arrow.now('PRC').datetime.replace(tzinfo=None)
-    v = VehicleHttp(ip_addr=ip, info=info, create_time=n)
-    db.session.add(v)
-    db.session.commit()
-
 @app.route('/heart', methods=['POST'])
 def heart_post():
     #if not request.json:
     #    return jsonify({'message': 'Problems parsing JSON'}), 415
     try:
         logger.info(request.json)
-        #add_vehicle_http(request.headers.get("X-Real-IP", request.remote_addr), request.json)
         data = "{0},host={1},serialno={2} value={3}".format('heart', request.headers.get("X-Real-IP", request.remote_addr), request.json['heartbeat']['serialno'], request.json['heartbeat']['countid'])
         helper.write_info(app.config['INFLUXDB_URL'], data)
         if request.json.get('heartbeat', None) is not None:
-            dev = get_device_state_by_ip(request.headers.get("X-Real-IP", request.remote_addr), request.json['heartbeat']['serialno'])
+            dev = set_device_state_by_ip(request.headers.get("X-Real-IP", request.remote_addr), request.json['heartbeat']['serialno'])
     except Exception as e:
         logger.error(e)
-    return {}, 201, {'Content-Type': 'application/json'}
+    return jsonify({}), 201, {'Content-Type': 'application/json'}
     #return jsonify(app.config['WHITE_LIST_DEL']), 201
