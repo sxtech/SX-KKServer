af701b4eb88453b6a049fc5a87d16dd7cc563944
SmellyCat <1282399562@qq.com>
2019-11-22 23:16:43 +0800

    更新 online日志使用msg

--- app/my_logger.py
+++ app/my_logger.py
@@ -27,10 +27,10 @@ def online_logging(log_file_name):
     if not os.path.isdir(path[0]):
         os.makedirs(path[0])
 
-    logger = logging.getLogger('root')
+    logger = logging.getLogger('msg')
 
     rthandler = logging.handlers.RotatingFileHandler(
-        log_file_name, maxBytes=20 * 1024 * 1024, backupCount=5, encoding='utf-8')
+        log_file_name, maxBytes=100 * 1024 * 1024, backupCount=5, encoding='utf-8')
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter(
         '%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -47,6 +47,6 @@ def access_logging(log_file_name):
     access_logger = logging.getLogger('access')
 
     rthandler = logging.handlers.RotatingFileHandler(
-        log_file_name, maxBytes=100 * 1024 * 1024, backupCount=10, encoding='utf-8')
+        log_file_name, maxBytes=100 * 1024 * 1024, backupCount=5, encoding='utf-8')
     access_logger.setLevel(logging.INFO)
     access_logger.addHandler(rthandler)
