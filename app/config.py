e9da416bc3b4cb0b1ce6f8a04a143344cdb80044
SmellyCat <1282399562@qq.com>
2019-12-03 10:48:23 +0800

    添加 注释

--- app/config.py
+++ app/config.py
@@ -6,26 +6,27 @@ class Config(object):
     # 服务器名称 string
     HEADER_SERVER = 'SX-KKServer'
     # 数据库连接 string
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123@127.0.0.1:54321/site'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123123@127.0.0.1:54321/site'
     # 数据库连接 dict
     SQLALCHEMY_BINDS = {
-        'env': 'mysql+pymysql://root:123@127.0.0.1:3307/environ'
+        'env': 'mysql+pymysql://root:123123@127.0.0.1:3307/environ'
     }
     # 连接池大小 int
     #SQLALCHEMY_POOL_SIZE = 20
-    # 基础路径
+    # 基础路径 string
     BASE_PATH = '/home/images'
-    # 基础URL路径
+    # 基础URL路径 string
     BASE_URL_PATH = 'http://112.91.72.23:8099/images'
-    # 时序数据库地址
+    # 时序数据库地址 string
     INFLUXDB_URL = 'http://localhost:8086/write?db=mz'
-
+    # pulsar配置信息 dict
     PULSAR = {
         'url': 'pulsar://localhost:6651',
         'topic': 'persistent://yc/mz/vehicle-pass',
         'client': None,
         'producer': None
     }
+
 
 class Develop(Config):
     DEBUG = True
